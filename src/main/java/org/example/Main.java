7e7fb09825f2760b0ab4279b355180317d0dbaf3
Junsu-Ahn <ajunsu5@gmail.com>
2024-03-19 18:53:12 +0900

    scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("== 프로젝트 시작 ==");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("입력 : ");
+
+        int i = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x + y + i);
+
+        sc.close();
+
+        System.out.println("== 프로젝트 끝 ==");
+
     }
 }
